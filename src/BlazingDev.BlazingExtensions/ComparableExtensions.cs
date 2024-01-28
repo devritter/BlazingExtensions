2f2fc75e79d64dfef150abf57baaee1d21d412a0
David Ritter <blazingdev@outlook.com>
2024-01-28 23:13:27 +0100

    LimitTo code doc

--- src/BlazingDev.BlazingExtensions/ComparableExtensions.cs
+++ src/BlazingDev.BlazingExtensions/ComparableExtensions.cs
@@ -4,6 +4,15 @@ namespace BlazingDev.BlazingExtensions;
 
 public static class ComparableExtensions
 {
+    /// <summary>
+    /// Limits/clamps the given "value" into the boundaries defined by "minValue" and "maxValue". <br/>
+    /// You can only define one boundary if you like.
+    /// </summary>
+    /// <param name="value">the value which gets compared/limited/clamped</param>
+    /// <param name="minValue">the lower boundary</param>
+    /// <param name="maxValue">the upper boundary</param>
+    /// <returns>the value raised to the "minValue", lowered to the "maxValue", or as-is if it has already been inside the range</returns>
+    /// <exception cref="ArgumentException">if the "minValue" is larger than the "maxValue"</exception>
     public static T LimitTo<T>(this T value, T? minValue, T? maxValue)
         where T : struct, IComparable
     {
