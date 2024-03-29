1fb424ff0ad7df7b3ac2bb48d77ccced93c00636
David Ritter <blazingdev@outlook.com>
2024-03-29 21:05:54 +0100

    code comments for OrderByFirstWhere() and WhereNotNull()

--- src/BlazingDev.BlazingExtensions/BzEnumerableExtensions.cs
+++ src/BlazingDev.BlazingExtensions/BzEnumerableExtensions.cs
@@ -29,22 +29,40 @@ public static class BzEnumerableExtensions
         return string.Join(separator, items);
     }
 
+    /// <summary>
+    /// Orders the "items" in a way that entries are on top where the "predicate" function returns true
+    /// </summary>
+    /// <param name="items">items that should be sorted</param>
+    /// <param name="predicate">function which returns true for the entries that should be on top</param>
     public static IOrderedEnumerable<T> OrderByFirstWhere<T>(this IEnumerable<T> items, Predicate<T> predicate)
     {
         return items.OrderBy(x => predicate(x) ? 0 : 1);
     }
 
+    /// <summary>
+    /// Further orders the "items" in a way that entries are on top where the "predicate" function returns true
+    /// </summary>
+    /// <param name="items">items that should be sorted</param>
+    /// <param name="predicate">function which returns true for the entries that should be on top</param>
     public static IOrderedEnumerable<T> ThenByFirstWhere<T>(this IOrderedEnumerable<T> items, Predicate<T> predicate)
     {
         return items.ThenBy(x => predicate(x) ? 0 : 1);
     }
 
+    /// <summary>
+    /// Filters out null items and fixes the nullability information for the compiler.
+    /// </summary>
+    /// <param name="items">collection with possible null items</param>
     public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> items)
         where T : class
     {
         return items.Where(x => x != null)!;
     }
 
+    /// <summary>
+    /// Filters out nullables without a value items and unpacks the value.
+    /// </summary>
+    /// <param name="items">collection with possible null items</param>
     public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> items)
         where T : struct
     {
