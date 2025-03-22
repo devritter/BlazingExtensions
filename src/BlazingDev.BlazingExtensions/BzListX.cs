2a00d18b0c0df755cef7c87190fc48029a170d9f
David Ritter <devritter@hotmail.com>
2025-03-22 21:48:39 +0100

    List.Extract(): add C# doc

--- src/BlazingDev.BlazingExtensions/BzListX.cs
+++ src/BlazingDev.BlazingExtensions/BzListX.cs
@@ -6,6 +6,12 @@ namespace BlazingDev.BlazingExtensions;
 
 public static class BzListX
 {
+    /// <summary>
+    /// Searches for items in the list, collects them in a dedicated list, and removes them from the source list.
+    /// Finally returns the collected (= removed) items.
+    /// </summary>
+    /// <param name="list">source list where matching items will be removed</param>
+    /// <param name="predicate">condition to find the items which should be removed from the source list</param>
     public static List<T> Extract<T>(this List<T> list, Func<T, bool> predicate)
     {
         var extracted = new List<T>();
